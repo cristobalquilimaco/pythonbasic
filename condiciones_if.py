3196881fd5a34deca56c97766f96743facb3a873
Cristobal <quilimacox1@gmail.com>
2024-07-25 18:37:47 -0500

    Se agrega condicion con booleanos

--- condiciones_if.py
+++ condiciones_if.py
@@ -1,4 +1,4 @@
-a = 2 
+"""a = 2 
 b = 3
 
 if a < b:
@@ -6,4 +6,11 @@ if a < b:
 elif a == b:
     print("A es igual a b")
 else:
-    print("A es mayor que b")
+    print("A es mayor que b")"""
+
+a = True
+
+if a:
+    print("A es verdadero")
+else:
+    print("B es falso")
