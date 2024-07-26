cacaae9bf507d4759dbffd283e5acecf513c61ef
Cristobal <quilimacox1@gmail.com>
2024-07-25 19:27:55 -0500

    Se condicion con varibles mayores

--- ciclo_for.py
+++ ciclo_for.py
@@ -2,7 +2,7 @@
 
 Imprime los números del 1 al 10 (inclusive) en una sola línea, separados por comas."""
 
-for num in range(1, 11):
+"""for num in range(1, 11):
     if num < 10:
         print(num, end=",")
     else:
@@ -13,4 +13,42 @@ for num in range(1, 11):
     if num <= 10:
         print(num + 1, end=",")
     else:
-        print(num)
+        print(num)"""
+
+"""frutas = {"fresa": "roja", "manzana": "verde", "plátano" : "amarillo"}
+
+for fruta in frutas.keys():
+    print(fruta)"""
+
+"""x = 0
+y = 0 
+z = 50
+
+while x < z:
+    x = y * 15
+    print(x)
+    y += 2
+
+print("fin de instrucción while")"""
+
+"""frutas = ["manzana", "naranja", "piña"]
+for fruta in frutas:
+    x = 0
+    for letra in fruta:
+        x += 1
+    print("fruta: ", fruta)
+    print("tiene: ", x, " letras")"""
+
+"""a = 10
+b = 2
+c = 3
+
+if a > b:
+   if b > c:
+      print(b)
+elif b > c:
+  print(c)
+if c > b:
+  print(a)
+else:
+  print(a, b, c)"""
